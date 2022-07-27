8cfc62d235a416a490f950aa33c3e36ee57f01da
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-07-27 13:46:59 +0300

    adds files in footer component

---
+++ src/Components/Header.js
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types'
+import { useLocation } from 'react-router-dom'
+
+import Button from './Elements/Button'
+
+const Header = ({ title, onAdd, showAdd }) => {
+    const location = useLocation()
+
+    return (
+        <header className='header'>
+            <h1>{ title }</h1>
+            {
+                location.pathname === '/' ? 
+                <Button 
+                color={!showAdd ? 'green': 'red'} 
+                text={!showAdd ? 'Add': 'Close'} 
+                onClick={ () => onAdd() } 
+                /> : ''
+            }
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title: 'Temp Title'
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired
+}
+
+
+// CSS in JSX
+// const headingStyle = {
+//     color: 'red', 
+//     textAlign: 'center'
+// }
+
+export default Header
