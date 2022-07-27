58671d4484d4055dec7ce03994c01b99830f572e
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-07-27 14:10:38 +0300

    mports Button.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,10 @@
 import PropTypes from 'prop-types'
+import { useLocation } from 'react-router-dom'
+
+import Button from './Elements/Button'
+
+const Header = ({ title, onAdd, showAdd }) => {
+    const location = useLocation()
 
     return (
         <header className='header'>
