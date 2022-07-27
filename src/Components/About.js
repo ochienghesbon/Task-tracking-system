a9da3aec4b13c8bf0974915e8406a4684df83bd8
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-07-27 16:28:20 +0300

    styles about comp

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 const About = () => {
     return (
         <div>
-            <h4>This is a Task Tracking System<a href="www.hesbon.com" target="_blank" style={{color: "white", textDecoration: "none"}}><b> Hesbon Odhiambo</b></a></h4>
+            <h4>This is a Task Tracking System<a href="www.hesbon.com" target="_self" style={{color: "white", textDecoration: "none"}}><b> Hesbon Odhiambo</b></a></h4>
             <Link to="/" style={{color:"white"}}>Home</Link>
         </div>
     )
