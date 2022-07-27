3b9ba2e83fe6f3442b6ea6270f97412a870b1fc5
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-07-27 13:14:52 +0300

    adds files in app component

--- src/App.js
+++ src/App.js
@@ -26,7 +26,76 @@ function App() {
   }
 
 
-  //
+  // Fetch one task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:8000/tasks/${id}`);
+    const data = await res.json();
+    return data;
+  }
+
+
+
+  // Add taks
+  const addTask = async (task) => {
+    const res = await fetch(`http://localhost:8000/tasks`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(task)
+    });
+
+    const data = await res.json();
+    setTasks([...tasks, data]);
+
+    // const id = Math.floor(Math.random() * 10000) + 1;
+    // const newTask = {id, ...task};
+    // setTasks([...tasks, newTask])
+  }
+
+
+  // Delete Task ----> PROP DRILLING
+  const delTask = async (id) => {
+    await fetch(`http://localhost:8000/tasks/${id}`, {
+      method: "DELETE"
+    });
+
+    // setTasks(tasks.filter((curTask) => {
+    //   if(curTask.id !== id)
+    //     return curTask;
+    // }))
+    setTasks(tasks.filter((curTask) =>(curTask.id !== id)) )
+  };
+  
+    
+
+
+  // Toggle Reminder ----> PROP DRILLING
+  const toggleReminder = async (id) => {
+
+    const taskToToggle = await fetchTask(id);
+
+    const updatedTask = {
+      ...taskToToggle, 
+      reminder: !taskToToggle.reminder
+    }
+
+    const res = await fetch(`http://localhost:8000/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(updatedTask)
+    });
+    const data = await res.json();
+
+    setTasks(tasks.map((task) => 
+      task.id === id ? 
+      {...task, reminder: data.reminder} : task
+    ))
+  }
+
+
 
   return (
     <Router>
