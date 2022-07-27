9537d8ef449d98a69344cebbb468553937802454
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-07-27 13:32:41 +0300

    imports header and button comonents to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Route, } from 'react-router-dom'
 
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 
 function App() {
